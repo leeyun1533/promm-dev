ae6063ad94ffcc27020e24140cd2b084ae4caf6a
jeongyun.lee <jeongyun.lee@linecorp.com>
2021-07-04 16:13:26 +0900

    fix: fix the filter bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,13 +13,15 @@ import titleIcon from '../images/title-icon.svg';
 import '../styles/pages/index.scss';
 
 const BlogIndex = ({ data, location }) => {
-  const posts = data.allMarkdownRemark.nodes;
   const categories: string[] = ['All', ...data.allMarkdownRemark.group.map((item) => item.fieldValue)];
   const [category, setCategory] = useState<string>((parse(globalHistory.location.search)?.category as string) || 'All');
+  const posts = data.allMarkdownRemark.nodes.filter((post) =>
+    category === 'All' ? post : category === post.frontmatter.category,
+  );
 
   const getThumbnail = (postIndex) => {
     const regex = /<img[^>]+src\s*=\s*['"]([^'"]+)['"][^>]*>/g;
-    const htmlString = data.allMarkdownRemark.nodes.map((htmlCode, htmlIndex) => {
+    const htmlString = posts.map((htmlCode, htmlIndex) => {
       if (postIndex === htmlIndex) {
         return htmlCode.html;
       }
@@ -46,7 +48,7 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location}>
-      <SEO title="All posts" />
+      <SEO title="promm devlog" />
       <div className="main">
         <aside className="aside main-aside">
           <Category categories={categories} category={category} setCategory={setCategory} />
